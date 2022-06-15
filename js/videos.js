8f40abbbcc754e802dfc2e85629baf605c5449ac
floaty Horizon Official <86828421+floatyHorizonOfficial@users.noreply.github.com>
2022-06-15 17:03:58 +0200

    Add files via upload

--- js/videos.js
+++ js/videos.js
@@ -4,11 +4,13 @@ let clicked = 0;
 TempBtn.addEventListener("click", () => {
   if (clicked === 0) {
     let text = document.createElement("p");
-    let textNode = document.createTextNode("Never Gonna Give You Up!");
+    let textNode = document.createTextNode("Never Gonna Give You Up! - You've been trolled! hehe");
     text.appendChild(textNode);
     document.body.appendChild(text);
     clicked++;
+    TempBtn.textContent = "Don't Click Me";
   } else {
     alert("No u")
+    document.body.remove();
   }
 })
