7d082b1ffe3b564170480557748063c37f8d96bd
floaty Horizon Official <86828421+floatyHorizonOfficial@users.noreply.github.com>
2022-07-22 14:52:07 +0200

    Add files via upload

--- js/videos.js
+++ js/videos.js
@@ -1,21 +1,17 @@
-const TempBtn = document.getElementById('temp-btn');
 const v2022 = document.getElementById('year-2022');
+
+const signup = document.getElementById('SignUp');
+const upload = document.getElementById('Upload');
+
 let clicked = 0;
 
 v2022.addEventListener("click", () => {
   alert("ERROR: No videos (yet?) from 2022!")
 })
 
-TempBtn.addEventListener("click", () => {
-  if (clicked === 0) {
-    let text = document.createElement("p");
-    let textNode = document.createTextNode("Dis is a Secret!");
-    text.appendChild(textNode);
-    document.body.appendChild(text);
-    clicked++;
-    TempBtn.textContent = "Don't Click Me";
-  } else {
-    alert("No u, Say goodbye to this page!")
-    document.body.remove();
-  }
+signup.addEventListener("click", () => {
+  window.location.replace("https://forms.gle/sj7pCS7meUAfEEzk9")
+})
+upload.addEventListener("click", () => {
+  window.location.replace("https://forms.gle/WFjcP2p217off5WNA")
 })
